f14fe68aba88caccee56d6e078800fe3dc26a645
sudhir singh <sudhirtestingzone@gmail.com>
2016-09-05 11:33:51 +0530

    hjgjhghj

---
+++ src/dddd/SSS.java
@@ -0,0 +1,22 @@
+package dddd;
+
+public class SSS {
+
+	public static void main(String[] args) {
+System.out.println("We are working with GitHub");
+
+int a,b,c;
+a=10;
+b=50;
+c=a+b;
+System.out.println(c);
+
+
+
+
+
+
+
+	}
+
+}
